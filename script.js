e235092d581e9a01180c8efb56f614b6f90124b9
szymon <szymon.borkowski@gmail.com>
2020-02-29 15:57:33 +0100

    add loader

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const hamburger_menu_button = document.querySelector(".hamburger--elastic");
 const menu = document.querySelector("aside")
 
-
+$(window).on('load', function () {
+    $('.loader-wrapper').fadeOut('slow');
+})
 hamburger_menu_button.addEventListener('click', function () {
 
     hamburger_menu_button.classList.toggle('is-active');
